d49ac406a4ec12fa85b2c222dd56512ad8bc7682
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-06-12 09:05:17 +0600

    try to solved-5 sign-in page.tsx href typeof problem

--- client/src/workArea/components/CommonButton/CommonButton.tsx
+++ client/src/workArea/components/CommonButton/CommonButton.tsx
@@ -1,20 +1,28 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { FC } from 'react';
-
+import { UrlObject } from 'url';
 
 interface CommonButtonProps {
     buttonText: string;
     buttonType?: string;
-    buttonGo?: string;
+    buttonGo?: string | UrlObject;
 }
 
 const CommonButton: FC<CommonButtonProps> = ({ buttonText, buttonType, buttonGo }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        if (buttonType === 'Link' && typeof buttonGo === 'string') {
+            router.push(buttonGo);
+        }
+    };
+
     return (
         <div className="mt-8 text-center">
             {buttonType === 'Link' ? (
-                <Link href={`${buttonGo}`} className=" bg-green-600 hover:bg-green-500 text-white py-4 px-6 rounded-lg uppercase">
+                <button className="bg-green-600 hover:bg-green-500 text-white py-4 px-6 rounded-lg uppercase" onClick={handleClick}>
                     {buttonText}
-                </Link>
+                </button>
             ) : (
                 <button className="bg-green-600 hover:bg-green-500 text-white py-4 px-6 rounded-lg uppercase">
                     {buttonText}
