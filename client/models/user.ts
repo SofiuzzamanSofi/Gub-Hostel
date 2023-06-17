6957eaa6a4e1070ecb68156104e8e9384381e7a7
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-06-18 02:23:28 +0600

    user schema create on models.User

--- client/models/user.ts
+++ client/models/user.ts
@@ -1,1 +1,25 @@
 import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: [true, "Please enter a valid email"],
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid Email Address"],
+    },
+    fullName: {
+        type: String,
+        required: [true, 'Name must be provided'],
+        minLength: [4, 'Full Name must be at least 4 characters'],
+        maxLength: [30, 'Full Name must not be greater than 30 characters long'],
+    },
+    password: {
+        type: String,
+        required: [true, "Password must be required"],
+        select: false,
+    }
+});
+
+const User = models.User || model('User', UserSchema);
+
+export default User;
