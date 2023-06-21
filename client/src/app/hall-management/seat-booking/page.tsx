14d79784906d439984e66e67937df68ed16e2fa9
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-06-21 06:38:21 +0600

    seat booking form is ready for client object data

--- client/src/app/hall-management/seat-booking/page.tsx
+++ client/src/app/hall-management/seat-booking/page.tsx
@@ -19,12 +19,28 @@ const SeatBooking: FC = () => {
     const router = useRouter();
     const searchParams = useSearchParams()
 
-    const [studentId, setStudentId] = useState('');
-    const [studentName, setStudentName] = useState('');
-    const [department, setDepartment] = useState('');
+
+    const [studentName, setStudentName] = useState(
+        searchParams?.get('fullName') ?? ''
+    );
+    const [mail, setMail] = useState(
+        searchParams?.get('email') ?? ''
+    );
+    const [mobile, setMobile] = useState(
+        searchParams?.get('mobile') ?? ''
+    );
+    const [studentId, setStudentId] = useState(
+        searchParams?.get('studentId') ?? ''
+    );
+    const [regular, setRegular] = useState('');
     const [semester, setSemester] = useState('');
-    const [mobile, setMobile] = useState('');
-    const [mail, setMail] = useState('');
+    const [semesterYear, setSemesterYear] = useState('');
+    const [hall, setHall] = useState('');
+    const [level, setLevel] = useState('');
+    const [room, setRoom] = useState('');
+    const [submitError, setSubmitError] = useState('');
+    const [loading, setLoading] = useState(false);
+
 
 
     const pathname = usePathname() ?? '';
@@ -34,12 +50,57 @@ const SeatBooking: FC = () => {
     const handleSeatBooking = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
 
+
+        // validate form
+        if (
+            studentName.trim() === '' ||
+            mail.trim() === '' ||
+            mobile.trim() === '' ||
+            studentId.trim() === '' ||
+            regular.trim() === '' ||
+            semester.trim() === '' ||
+            semesterYear.trim() === '' ||
+            hall.trim() === '' ||
+            level.trim() === '' ||
+            room.trim() === ''
+        ) {
+
+            // Display an error message or handle the validation failure
+            return toast.error('Please fill out all fields properly');
+        }
+
+        setLoading(true);
+        const handleSeatBookingData = {
+            fullName: studentName,
+            email: mail,
+            mobile,
+            studentId,
+            regular,
+            semester,
+            semesterYear,
+            hall,
+            level,
+            room,
+        }
+        console.log("handleSeatBookingData on 66:", handleSeatBookingData)
+
+
         toast.success('Successfully toasted!')
-        router.push("/hall-management");
+        // router.push("/hall-management");
+
+        setLoading(false);
     };
 
-    const email = searchParams?.get('email') ?? '';
-    console.log("seat booking from page line 45:", email)
+    // const email = searchParams?.get('email') ?? '';
+    // const fullName = searchParams?.get('fullName') ?? '';
+    // const mobileFromDB = searchParams?.get('mobile') ?? '';
+    // const studentIdFromDB = searchParams?.get('studentId') ?? '';
+    // setStudentName(fullName);
+    // setMail(email);
+    // setMobile(mobileFromDB);
+    // setStudentId(studentIdFromDB);
+
+    // console.log("seat booking from page line 45:", email)
 
     return (
         <div className='my-12'>
@@ -72,6 +133,7 @@ const SeatBooking: FC = () => {
                                         placeholder="Student Name"
                                         value={studentName}
                                         onChange={(e) => setStudentName(e.target.value)}
+                                        required
                                     />
 
                                     <input
@@ -81,15 +143,17 @@ const SeatBooking: FC = () => {
                                         placeholder="Eail"
                                         value={mail}
                                         onChange={(e) => setMail(e.target.value)}
+                                        required
                                     />
 
                                     <input
                                         className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center"
-                                        type="text"
+                                        type="number"
                                         name="mobile"
                                         placeholder="Mobile No"
                                         value={mobile}
                                         onChange={(e) => setMobile(e.target.value)}
+                                        required
                                     />
 
                                     <input
@@ -99,43 +163,67 @@ const SeatBooking: FC = () => {
                                         placeholder="Student ID"
                                         value={studentId}
                                         onChange={(e) => setStudentId(e.target.value)}
+                                        required
                                     />
 
 
-                                    <div
-                                        className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center"
-                                    >
-                                        <label htmlFor="studentType">Student Type <span className="text-red-500">*</span> </label>
-                                        <select name="studentType" id="studentType" className="bg-[#CDCDCD] dark:bg-gray-700 text-center">
-                                            <option disabled value="">Chose One</option>
-                                            <option value="regular">Regular</option>
-                                            <option value="irRegular">Ir Regular</option>
+                                    <div className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center">
+                                        <label htmlFor="studentType">
+                                            Student Type <span className="text-red-500">*</span>{' '}
+                                        </label>
+                                        <select
+                                            name="studentType"
+                                            id="studentType"
+                                            className="bg-[#CDCDCD] dark:bg-gray-700 text-center"
+                                            value={regular}
+                                            onChange={(e) => setRegular(e.target.value)}
+                                            required
+                                        >
+                                            <option disabled >Choose One</option>
+                                            <option value="Regular">Regular</option>
+                                            <option value="Irregular">Irregular</option>
                                         </select>
                                     </div>
 
-                                    <div
-                                        className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center"
-                                    >
-                                        <label htmlFor="semesterType">Semester<span className="text-red-500">*</span> </label>
-                                        <select name="semesterType" id="semesterType" className="bg-[#CDCDCD] dark:bg-gray-700 text-center">
-                                            <option disabled value="">Chose One</option>
-                                            <option value="first">First</option>
-                                            <option value="second">Second</option>
+                                    <div className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center">
+                                        <label htmlFor="semesterType">
+                                            Semester<span className="text-red-500">*</span>{' '}
+                                        </label>
+                                        <select
+                                            name="semesterType"
+                                            id="semesterType"
+                                            className="bg-[#CDCDCD] dark:bg-gray-700 text-center"
+                                            value={semester}
+                                            onChange={(e) => setSemester(e.target.value)}
+                                        >
+                                            <option disabled value="">
+                                                Choose One
+                                            </option>
+                                            <option value="First">First</option>
+                                            <option value="Second">Second</option>
                                             <option value="Third">Third</option>
-                                            <option value="fourth">Fourth</option>
-                                            <option value="fifth">Fifth</option>
-                                            <option value="sixth">Sixth</option>
-                                            <option value="seventh">Seventh</option>
-                                            <option value="eighth">Eighth</option>
+                                            <option value="Fourth">Fourth</option>
+                                            <option value="Fifth">Fifth</option>
+                                            <option value="Sixth">Sixth</option>
+                                            <option value="Seventh">Seventh</option>
+                                            <option value="Eighth">Eighth</option>
                                         </select>
                                     </div>
 
-                                    <div
-                                        className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center"
-                                    >
-                                        <label htmlFor="semesterYear">Semester Year<span className="text-red-500">*</span> </label>
-                                        <select name="semesterYear" id="semesterYear" className="bg-[#CDCDCD] dark:bg-gray-700 text-center">
-                                            <option disabled value="">Chose One</option>
+                                    <div className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center">
+                                        <label htmlFor="semesterYear">
+                                            Semester Year<span className="text-red-500">*</span>{' '}
+                                        </label>
+                                        <select
+                                            name="semesterYear"
+                                            id="semesterYear"
+                                            className="bg-[#CDCDCD] dark:bg-gray-700 text-center"
+                                            value={semesterYear}
+                                            onChange={(e) => setSemesterYear(e.target.value)}
+                                        >
+                                            <option disabled value="">
+                                                Choose One
+                                            </option>
                                             <option value="2019">2019</option>
                                             <option value="2020">2020</option>
                                             <option value="2021">2021</option>
@@ -148,9 +236,19 @@ const SeatBooking: FC = () => {
                                     </div>
 
                                     <div className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center">
-                                        <label htmlFor="hall">Hall<span className="text-red-500">*</span> </label>
-                                        <select name="hall" id="hall" className="bg-[#CDCDCD] dark:bg-gray-700 text-center">
-                                            <option disabled value="">Choose One</option>
+                                        <label htmlFor="hall">
+                                            Hall<span className="text-red-500">*</span>{' '}
+                                        </label>
+                                        <select
+                                            name="hall"
+                                            id="hall"
+                                            className="bg-[#CDCDCD] dark:bg-gray-700 text-center"
+                                            value={hall}
+                                            onChange={(e) => setHall(e.target.value)}
+                                        >
+                                            <option disabled value="">
+                                                Choose One
+                                            </option>
                                             <option value="Shahid Jiya Hall">Shahid Jiya Hall</option>
                                             <option value="Mawlana Vasani Hall">Mawlana Vasani Hall</option>
                                             <option value="Shere E Bangla Hall">Shere E Bangla Hall</option>
@@ -163,29 +261,41 @@ const SeatBooking: FC = () => {
                                     </div>
 
 
-                                    <div
-                                        className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center"
-                                    >
-                                        <label htmlFor="level">Level<span className="text-red-500">*</span> </label>
-                                        <select name="level" id="level" className="bg-[#CDCDCD] dark:bg-gray-700 text-center">
+                                    <div className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center">
+                                        <label htmlFor="level">
+                                            Level<span className="text-red-500">*</span>{' '}
+                                        </label>
+                                        <select
+                                            name="level"
+                                            id="level"
+                                            className="bg-[#CDCDCD] dark:bg-gray-700 text-center"
+                                            value={level}
+                                            onChange={(e) => setLevel(e.target.value)}
+                                        >
                                             <option disabled value="">Chose One</option>
-                                            <option value="first">First</option>
-                                            <option value="second">Second</option>
+                                            <option value="First">First</option>
+                                            <option value="Second">Second</option>
                                             <option value="Third">Third</option>
-                                            <option value="fourth">Fourth</option>
-                                            <option value="fifth">Fifth</option>
-                                            <option value="sixth">Sixth</option>
-                                            <option value="seventh">Seventh</option>
-                                            <option value="eighth">Eighth</option>
+                                            <option value="Fourth">Fourth</option>
+                                            <option value="Fifth">Fifth</option>
+                                            <option value="Sixth">Sixth</option>
+                                            <option value="Seventh">Seventh</option>
+                                            <option value="Eighth">Eighth</option>
                                         </select>
                                     </div>
 
 
-                                    <div
-                                        className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center"
-                                    >
-                                        <label htmlFor="room">Room<span className="text-red-500">*</span> </label>
-                                        <select name="room" id="room" className="bg-[#CDCDCD] dark:bg-gray-700 text-center">
+                                    <div className="my-4 p-4 rounded-md bg-[#CDCDCD] dark:bg-gray-700 dark:text-white text-center">
+                                        <label htmlFor="room">
+                                            Room<span className="text-red-500">*</span>{' '}
+                                        </label>
+                                        <select
+                                            name="room"
+                                            id="room"
+                                            className="bg-[#CDCDCD] dark:bg-gray-700 text-center"
+                                            value={room}
+                                            onChange={(e) => setRoom(e.target.value)}
+                                        >
                                             <option disabled value="">Chose One</option>
                                             <option value="101">101</option>
                                             <option value="102">102</option>
@@ -217,12 +327,18 @@ const SeatBooking: FC = () => {
 
                                 </div>
                                 <div>
+                                    {
+                                        submitError &&
+                                        <p className='text-sm text-red-600'>{submitError}</p>
+                                    }
+                                </div>
+                                <div>
 
                                     <p className="text-sm">Star(*) Marked are mandatory fields.</p>
                                 </div>
                                 <div>
-                                    <CommonButton buttonText="Submit" />
-                                    {/* <input type="submit" value="Sign Up" /> */}
+                                    <CommonButton buttonText={`${loading ? "Loading..." : "Submit"}`} isDisabled={loading ? true : false} />
+
                                 </div>
                             </form>
 
